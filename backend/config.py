c9b3888bca74abe2142d6f5c17b549fbfdc75f87
nizhenshi <zmc_li@foxmail.com>
2025-08-23 17:19:54 +0800

    修改测试环境连接数据库的配置

--- backend/config.py
+++ backend/config.py
@@ -68,7 +68,7 @@ class TestingConfig(Config):
     DEBUG = True
     # mysql
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-                              'mysql+pymysql://root:12341234@127.0.0.1:3306/test_backend_flask?charset=utf8mb4'
+                              'mysql+pymysql://root:1234@127.0.0.1:3306/test_backend_flask?charset=utf8mb4'
     # redis
     REDIS_URL = os.environ.get('TEST_REDIS_URL') or "redis://127.0.0.1:6379/0"  # 格式：redis://:<password>@<host>:<port>/<db>
     WTF_CSRF_ENABLED = False
@@ -77,7 +77,7 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     # mysql
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'mysql+pymysql://root:12341234@' + os.getenv('FLASK_RUN_HOST',
+                              'mysql+pymysql://root:1234@' + os.getenv('FLASK_RUN_HOST',
                                                                                   '') + ':3306/backend_flask?charset=utf8mb4'
     # redis
     REDIS_URL = os.environ.get('REDIS_URL') or "redis://:1234@" + os.getenv('FLASK_RUN_HOST',
