5d452df08116f476f94a2a67263c656f69269be8
nizhenshi <zmc_li@foxmail.com>
2025-09-05 23:08:39 +0800

    Fix JWT user lookup callback parameter name

--- backend/app/models.py
+++ backend/app/models.py
@@ -450,9 +450,9 @@ def user_identify_lookup(user):
 
 
 @jwt.user_lookup_loader
-def user_lookup_callback(__jwt_header, jwt_data):
-    identify = jwt_data["sub"]
-    return User.query.filter_by(id=identify).one_or_none()
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
 
 
 class PostType(Enum):
