eb7bacdfa17486091fcc6d5ac0ba75734cae6d55
zmc <2797139657@qq.com>
2025-11-26 17:10:36 +0800

    移除前端的首屏优化

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,20 @@ import pinia from "./stores/index.js";
 import dayjs from "./config/dayjsCfg";
 
 // 样式导入
-import { loadNonCriticalStyles } from "@/utils/loadNonCriticalStyles";
-import { lazyInstallUIFrameworks } from "@/utils/lazyInstallUIFrameworks";
+import "@wangeditor/editor/dist/css/style.css";
+import "element-plus/dist/index.css";
+import "vant/lib/index.css";
+import "undraw-ui/dist/style.css";
+import "mavon-editor/dist/css/index.css";
 
 // 插件导入
+import { ElMessage } from "element-plus";
 import { loadingFadeOut } from "virtual:app-loading";
+import { useElementPlus } from "@/plugins/elementPlus";
+import { useVant } from "@/plugins/vant";
 import vSlideIn from "@/directives/vSlideIn.js";
 import { UIcon } from "undraw-ui";
+import mavonEditor from "mavon-editor";
 
 loadingFadeOut();
 
@@ -19,14 +26,7 @@ const app = createApp(App);
 
 // 全局属性
 app.config.globalProperties.$dayjs = dayjs;
-app.config.globalProperties.$message = (...args) => {
-  if (import.meta.env.DEV) {
-    console.warn(
-      "[lazy-ui] Element Plus 未加载完成，消息已被忽略。等待首次进入主站页面后即可正常使用。",
-      args
-    );
-  }
-};
+app.config.globalProperties.$message = ElMessage;
 
 // 指令注册
 app.directive("slide-in", vSlideIn);
@@ -37,9 +37,8 @@ app.component("u-icon", UIcon);
 // 插件使用
 app.use(pinia);
 app.use(router);
-
-lazyInstallUIFrameworks(app, router);
+app.use(useElementPlus);
+app.use(useVant);
+app.use(mavonEditor);
 
 app.mount("#app");
-
-loadNonCriticalStyles();
